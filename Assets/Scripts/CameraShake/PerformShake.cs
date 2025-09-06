70f6d698ac5589a23364707231c5cba3919713ee
Lin1ux <liniuxpl2@gmail.com>
2025-09-06 15:51:32 +0200

    add camera shake

---
+++ Assets/Scripts/CameraShake/PerformShake.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PerformShake : MonoBehaviour
+{
+    [SerializeField] private CameraShaker shaker;
+
+    [SerializeField] private float positionStrength = 1f; 
+    [SerializeField] private float rotationStrength = 3;
+    [SerializeField] private float duration = 0.5f;
+
+
+    public void TriggerShake(Vector2 desiredDirection)
+    {
+        CameraShaker.Presets.Explosion2D(positionStrength,rotationStrength,duration);
+    }
+
+    void Update()
+    {
+        //TriggerShake(transform.position);
+    }
+}

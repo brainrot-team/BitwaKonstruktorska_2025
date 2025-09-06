0381d7029f0a197d01558d59605adceadc0cabb5
rainot <olga24@onet.pl>
2025-09-06 15:07:31 +0200

    enemies droping on death

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -44,15 +44,32 @@ public class EnemyController : MonoBehaviour
 
     public void HitByProjectile()
     {
+        print("YOU are hit");
         currentHealth--;
-        if(currentHealth <= 0)
+        if (currentHealth <= 0)
+        {
             Destroy(gameObject);
+            SpawnProjectileOnDeath();
+        }
 
     }
 
     private void SpawnProjectileOnDeath()
     {
 
+        print("YOU ARE DEAD");
+
+        WorldManager.Instance.TrashScore++;
+        for(int i =0;i<enemyData.maxHP +1;i++)
+        {
+            
+            GameObject trashObject = TrashPrefabHolder.Instance.getRandomTrash();
+
+            var pom = ((float)i / (enemyData.maxHP +1)) * 2 * Mathf.PI;
+            trashObject.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(Mathf.Sin(pom),Mathf.Cos(pom));
+            trashObject.transform.SetPositionAndRotation(transform.position, transform.rotation);
+            trashObject.GetComponent<TrashProjectile>().ShootProjectile(ProjectileOrigin.neutral);
+        }
 
     }
 }

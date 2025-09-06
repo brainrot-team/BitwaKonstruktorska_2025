8d03a3b3902a80dabdbead857c337072e9161ec4
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2025-09-06 14:58:42 +0200

    add sprite flash

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -5,7 +6,7 @@ public class EnemyController : MonoBehaviour
 
     [HideInInspector] public GameObject enemyGameObject;
     [HideInInspector] public Rigidbody2D rb;
-    
+
     public EnemyViewRange viewRange;
     public StateMachine stateMachine;
 
@@ -15,6 +16,11 @@ public class EnemyController : MonoBehaviour
     public int lastMultiplayer = 0;
 
     public int currentHealth;
+
+    // Sprite flashing
+    public SpriteRenderer spriteRenderer { get; private set; }
+    private Material material;
+    private IEnumerator flashCoroutine;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,7 +32,9 @@ public class EnemyController : MonoBehaviour
         stateMachine.Init(states.ForwardState);
         //stateMachine.Init(states.ToCenterState);
         currentHealth = enemyData.maxHP;
-        
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        material = spriteRenderer.material;
     }
 
     void Update()
@@ -37,15 +45,15 @@ public class EnemyController : MonoBehaviour
     void FixedUpdate()
     {
         stateMachine.currentState.UpdatePhysics();
-    
-    
-    
+
+
+
     }
 
     public void HitByProjectile()
     {
         currentHealth--;
-        if(currentHealth <= 0)
+        if (currentHealth <= 0)
             Destroy(gameObject);
 
     }
@@ -54,5 +62,35 @@ public class EnemyController : MonoBehaviour
     {
 
 
+    }
+    
+    
+    public void FlashOnDamage()
+    {
+        if (flashCoroutine != null)
+            StopCoroutine(flashCoroutine);
+
+        flashCoroutine = FlashCoroutine();
+        StartCoroutine(flashCoroutine);
+    }
+
+    private IEnumerator FlashCoroutine()
+    {
+        float lerpTime = 0;
+
+        while (lerpTime < 0.3f)
+        {
+            lerpTime += Time.deltaTime;
+            float perc = lerpTime / 0.5f;
+
+            SetFlashAmount(1f - perc);
+            yield return null;
+        }
+        SetFlashAmount(0);
+    }
+
+    private void SetFlashAmount(float flashAmount)
+    {
+        material.SetFloat("_FlashAmount", flashAmount);
     }
 }
